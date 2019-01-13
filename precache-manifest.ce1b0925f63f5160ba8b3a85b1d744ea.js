35f6eb8b5b4b6076d3976f53a14f3273156cfeb0
Atanas Shtrankov <atanasdiyanov@gmail.com>
2019-01-13 12:15:41 +0200

    Initial dist subtree

---
+++ precache-manifest.ce1b0925f63f5160ba8b3a85b1d744ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3ed6554c6a8efc567f7",
+    "url": "/vue-pdp/js/about.1b2e55ae.js"
+  },
+  {
+    "revision": "3e340a6cfe7263dd9d18",
+    "url": "/vue-pdp/css/app.64571b26.css"
+  },
+  {
+    "revision": "3e340a6cfe7263dd9d18",
+    "url": "/vue-pdp/js/app.23bc4ded.js"
+  },
+  {
+    "revision": "40b0657bb4852d247a88",
+    "url": "/vue-pdp/js/chunk-vendors.8874ba86.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-pdp/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "dff49794ab8abeb6d1c02cb3042123f0",
+    "url": "/vue-pdp/img/main.dff49794.jpg"
+  },
+  {
+    "revision": "3654f21f6d0b2c46304a80e18dd1ce84",
+    "url": "/vue-pdp/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-pdp/robots.txt"
+  }
+];
