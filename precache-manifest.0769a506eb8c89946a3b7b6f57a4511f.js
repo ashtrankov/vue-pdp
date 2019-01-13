11267bb9016c17c97c341f34fe4f5125cc022024
Atanas Shtrankov <atanasdiyanov@gmail.com>
2019-01-13 13:15:21 +0200

    dist

---
+++ precache-manifest.0769a506eb8c89946a3b7b6f57a4511f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3ed6554c6a8efc567f7",
+    "url": "/vue-pdp/js/about.1b2e55ae.js"
+  },
+  {
+    "revision": "f667f660336f8dcabd25",
+    "url": "/vue-pdp/css/app.e3ede127.css"
+  },
+  {
+    "revision": "f667f660336f8dcabd25",
+    "url": "/vue-pdp/js/app.bca1f492.js"
+  },
+  {
+    "revision": "08672c9c8b921f69f6ec",
+    "url": "/vue-pdp/js/chunk-vendors.986bcc2f.js"
+  },
+  {
+    "revision": "38e8a863d9d347d3d7ba2c97f36a5eba",
+    "url": "/vue-pdp/img/main2.38e8a863.jpg"
+  },
+  {
+    "revision": "d115b5c7671321b722950a65b2124a8c",
+    "url": "/vue-pdp/img/main4.d115b5c7.jpg"
+  },
+  {
+    "revision": "4ed50e9d8463a2fca7901bffc7bbe5e4",
+    "url": "/vue-pdp/img/main3.4ed50e9d.jpg"
+  },
+  {
+    "revision": "5fa01ba7abe66698169ac466af613c6c",
+    "url": "/vue-pdp/img/main1.5fa01ba7.jpg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-pdp/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d115b5c7671321b722950a65b2124a8c",
+    "url": "/vue-pdp/img/main.d115b5c7.jpg"
+  },
+  {
+    "revision": "86dad95cecd12e7b2e40d32112407771",
+    "url": "/vue-pdp/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-pdp/robots.txt"
+  }
+];
